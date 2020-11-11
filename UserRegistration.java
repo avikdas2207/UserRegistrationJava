aed7fd314d89abbd336bbd2294ca4e0117d5df4a
avikdas2207 <avikdas3535@gmail.com>
2020-11-12 02:54:04 +0530

    last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -7,19 +7,38 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
 	static Scanner input = new Scanner (System.in);
+	//Variables
 	static String firstName;
+	static String lastName;
 	
 	//Regex patterns
 	static String FIRST_NAME = "[A-Z]{1}[a-z]{3,}";
+	static String LAST_NAME = "[A-Z]{1}[a-z]{3,}";
 	
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcome to user registration program \nEnter first name");
+		System.out.println("Welcome to user registration program");
 		
+		System.out.println("Enter first name");
 		firstName = input.next();
+		System.out.println("Enter last name");
+		lastName = input .next();
+		
 		matchFirstName();
+		matchLastName();
 		input.close();
 		
+	}
+
+	private static void matchLastName() 
+	{
+		Pattern p = Pattern.compile(LAST_NAME);
+		Matcher m = p.matcher(lastName);
+		
+		if (m.find())
+			System.out.println("Last name is correct.");
+		else
+			System.out.println("Last name is incorrect.");
 	}
 
 	private static void matchFirstName() 
@@ -28,9 +47,9 @@ public class UserRegistration {
 		Matcher m = p.matcher(firstName);
 		
 		if (m.find())
-			System.out.println("Correct");
+			System.out.println("First name is Correct.");
 		else
-			System.out.println("incorrect");
+			System.out.println("First name is incorrect.");
 	}
 
 }
